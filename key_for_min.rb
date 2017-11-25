10a20804a0482d33831306160cbdcea4d9340ad4
Liz Burton <liz.burton147@gmail.com>
2017-11-25 14:12:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   min_value = 0
   name_hash.collect do | key, value |
     if min_value == 0 || min_value > value
-      key
+      min_value = value
     end
   end
+  key
 end
